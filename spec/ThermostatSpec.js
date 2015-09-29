07f34e898fdf933b2fe208c99e6e9ce7ae72a32f
chweeks <chrisharryweeks@gmail.com>
2015-09-29 14:15:36 +0100

    can increase and decrease temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,4 +8,14 @@ describe('Thermostat', function() {
   it('starts at 20 degrees when initialized', function() {
     expect(thermostat.temperature).toBe(20)
   });
+
+  it('increases temperature by one degree', function() {
+    thermostat.increase_temp();
+    expect(thermostat.temperature).toBe(21);
+  });
+
+  it('decreases temperature by one degree', function() {
+    thermostat.decrease_temp();
+    expect(thermostat.temperature).toBe(19)
+  });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,3 +1,11 @@
 function Thermostat() {
   this.temperature = 20;
 };
+
+Thermostat.prototype.increase_temp = function() {
+  this.temperature ++
+};
+
+Thermostat.prototype.decrease_temp = function() {
+  this.temperature --
+};
