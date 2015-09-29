18d9e656c59dea7438118f96c573a4ab11948791
chweeks <chrisharryweeks@gmail.com>
2015-09-29 15:51:37 +0100

    added power saving mode

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -10,20 +10,37 @@ describe('Thermostat', function() {
   });
 
   it('increases temperature by one degree', function() {
-    thermostat.increase_temp();
+    thermostat.increaseTemp();
     expect(thermostat.temperature).toBe(21);
   });
 
   it('decreases temperature by one degree', function() {
-    thermostat.decrease_temp();
+    thermostat.decreaseTemp();
     expect(thermostat.temperature).toBe(19)
   });
+
   it('has a minimum temperature of 10 degrees', function() {
     expect(thermostat.minimum).toBe(10)
   })
+
   it('cannot go below minimum temperature', function() {
     thermostat.temperature = 10;
-    expect(function(){thermostat.decrease_temp();}).toThrow('Cannot go below the minimum')
+    expect(function(){thermostat.decreaseTemp();}).toThrow('Cannot go below the minimum')
   })
+
+  it('starts in power saving mode', function() {
+    expect(thermostat.isPowerSaving).toBe(true)
+  });
+
+  it('can turn power saving mode off', function() {
+    thermostat.powerSavingOff()
+    expect(thermostat.isPowerSaving).toBe(false)
+  });
+
+  it('can turn power saving mode on', function() {
+    thermostat.powerSavingOff()
+    thermostat.powerSavingOn()
+    expect(thermostat.isPowerSaving).toBe(true)
+  });
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,16 +1,25 @@
 function Thermostat() {
   this.temperature = 20;
   this.minimum = 10;
+  this.isPowerSaving = true
 };
 
-Thermostat.prototype.increase_temp = function() {
+Thermostat.prototype.increaseTemp = function() {
   this.temperature ++
 };
 
-Thermostat.prototype.decrease_temp = function() {
+Thermostat.prototype.decreaseTemp = function() {
   if (this.temperature > this.minimum) {
-    this.temperature --
-  } else {
+    this.temperature -- }
+  else {
     throw 'Cannot go below the minimum';
   }
+};
+
+Thermostat.prototype.powerSavingOff = function() {
+  this.isPowerSaving = false;
+};
+
+Thermostat.prototype.powerSavingOn = function() {
+  this.isPowerSaving = true;
 };
