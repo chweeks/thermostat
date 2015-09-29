0e41b47fb800dde0edd317af231124b24577d18f
chweeks <chrisharryweeks@gmail.com>
2015-09-29 16:47:02 +0100

    added jquery foundation

---
+++ application.js
@@ -0,0 +1,29 @@
+$( document ).ready (function() {
+  $( "#increase" ).click(function( event ) {
+
+        alert( "Clicked Increase!" );
+
+    });
+
+  $( "#decrease" ).click(function( event ) {
+
+        alert( "Clicked Decrease!" );
+
+    });
+
+  $( "#reset" ).click(function( event ) {
+
+        alert( "Clicked Reset!" );
+
+    });
+    $( "#powersaveon" ).click(function( event ) {
+
+              alert( "Clicked PowerSave On!" );
+
+          });
+    $( "#powersaveoff" ).click(function( event ) {
+
+          alert( "Clicke PowerSave Off!" );
+
+      });
+});
