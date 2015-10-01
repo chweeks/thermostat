fd07ebf44d89d451f59a8e1568819589bac9d582
chweeks <chrisharryweeks@gmail.com>
2015-10-01 17:34:01 +0100

    started implementing google images api

--- application.js
+++ application.js
@@ -8,15 +8,22 @@ $( document ).ready (function() {
   }
 
   var pathArray = window.location.search;
-    var urlCity = pathArray.replace('?city=', '').replace('&Submit=Submit', '');
-    var weathertemp;
-    var weathername;
-    $.ajax('http://api.openweathermap.org/data/2.5/weather?q=' + urlCity, {
-      success: function(data) {
-        weathertemp = Math.round(data.main.temp - 273.15);
-        weathername = data.name;
-        update();
-      }
+  var urlCity = pathArray.replace('?city=', '').replace('&Submit=Submit', '') || 'London';
+  var weathertemp;
+  var weathername;
+  $.ajax('http://api.openweathermap.org/data/2.5/weather?q=' + urlCity, {
+    success: function(data) {
+      weathertemp = Math.round(data.main.temp - 273.15);
+      weathername = data.name;
+      update();
+    }
+  });
+
+  $.ajax('https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=' + urlCity +'&userip=185.53.227.70', {
+    success: function(image) {
+      console.log(imgurl = image.responseData.results.url);
+      update();
+    }
   });
 
   update();
