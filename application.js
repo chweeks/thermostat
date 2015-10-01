8afe304ae109584f7c0e0156616bc743ac60b981
chweeks <chrisharryweeks@gmail.com>
2015-10-01 13:51:30 +0100

    added london weather

--- application.js
+++ application.js
@@ -2,11 +2,30 @@ $( document ).ready (function() {
 
   var update = function(){
     $( "#temp" ).text(thermostat.temperature);
+    $( "#weather" ).text(thermostat.weather);
     $( "#usage").attr('style','color:'+ thermostat.energyUsage());
   }
-
+  $('#weather_city').val('London')
+  weatherForm();
   update();
 
+  function weatherForm() {
+    $.ajax({
+      url: 'http://api.openweathermap.org/data/2.5/weather',
+      jsonp: 'callback',
+      dataType: 'jsonp',
+      cache: false,
+      data: {
+        q: $('#weather_city').val(),
+      },
+      // work with the response
+      success: function (response) {
+        $('#weather_description').text(response.weather[0].description);
+        $('#weather_temp').text(Math.round(((response.main.temp)-272)));
+        $('#weather_wind').text(response.wind.speed);
+      },
+    });
+  }
   $( "#increase" ).click(function( event ) {
 
     thermostat.increaseTemp();
